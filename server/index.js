ff93e3e53d27069ffb2bde316292c82b635a0eb9
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-07 01:48:38 +0300

    react init

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import TelegramApi from 'node-telegram-bot-api'
+import * as dotenv from 'dotenv'
+dotenv.config()
+const token = process.env.TOKEN
+const bot = new TelegramApi(token, { polling: true })
+const url = 'https://funny-kleicha-8671e1.netlify.app/'
+
+bot.setMyCommands([
+    { command: '/start', description: 'start' },
+    { command: '/info', description: 'info' }
+])
+const start = () => {
+    bot.on('message', async message => {
+        const { text, chat } = message
+        const chatId = chat.id
+        console.log(text, chat)
+        if (text === '/info') {
+            return bot.sendMessage(chatId, `Привет ${chat.first_name}`)
+        }
+        if (text === '/start') {
+            return bot.sendMessage(chatId, 'Выбирай список вопросов и качай свои скилы', {
+                reply_markup: {
+                    inline_keyboard: [
+                        [{ text: "HTML", web_app: { url } }, { text: "CSS", web_app: { url } }, { text: "JavaScript", web_app: { url } }],
+                        [{ text: "React", web_app: { url } }, { text: "Vue", web_app: { url } }]
+                    ]
+                }
+            })
+        }
+        return bot.sendMessage(chatId, '.....')
+    })
+}
+start()
