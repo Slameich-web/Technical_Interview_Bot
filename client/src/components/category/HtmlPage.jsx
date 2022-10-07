2ea8db67de7122eacaff4befd27767e98a6ad61b
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-07 03:25:47 +0300

    test antd

--- client/src/components/category/HtmlPage.jsx
+++ client/src/components/category/HtmlPage.jsx
@@ -1,5 +1,19 @@
 import React from "react";
-
+import "antd/dist/antd.css";
+import { Collapse } from "antd";
 export const HtmlPage = () => {
-  return <div>HtmlPage</div>;
+  const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+  return (
+    <div>
+      <h2>Добро пожаловать на страницу с вопросами по HTML</h2>
+      <h4>Тут ты можешь выбрать сложность вопросов и дополнительные темы</h4>
+      <Collapse header="This is panel header 1" key="1">
+        <p>{text}</p>
+      </Collapse>
+    </div>
+  );
 };
--- server/index.js
+++ server/index.js
@@ -4,11 +4,7 @@ dotenv.config()
 const token = process.env.TOKEN
 const bot = new TelegramApi(token, { polling: true })
 const url = 'https://cozy-concha-362660.netlify.app/'
-const urlHtml = 'https://cozy-concha-362660.netlify.app/html'
-const urlCss = 'https://cozy-concha-362660.netlify.app/css'
-const urlJs = 'https://cozy-concha-362660.netlify.app/javascript'
-const urlReact = 'https://cozy-concha-362660.netlify.app/react'
-const urlVue = 'https://cozy-concha-362660.netlify.app/vue'
+
 
 bot.setMyCommands([
     { command: '/start', description: 'start' },
@@ -18,7 +14,6 @@ const start = () => {
     bot.on('message', async message => {
         const { text, chat } = message
         const chatId = chat.id
-        console.log(text, chat)
         if (text === '/info') {
             return bot.sendMessage(chatId, `Привет ${chat.first_name}`)
         }
@@ -26,8 +21,8 @@ const start = () => {
             return bot.sendMessage(chatId, 'Выбирай список вопросов и качай свои скилы', {
                 reply_markup: {
                     inline_keyboard: [
-                        [{ text: "HTML", web_app: { urlHtml } }, { text: "CSS", web_app: { urlCss } }, { text: "JavaScript", web_app: { urlJs } }],
-                        [{ text: "React", web_app: { urlReact } }, { text: "Vue", web_app: { urlVue } }]
+                        [{ text: "HTML", web_app: { url: url + 'html' } }, { text: "CSS", web_app: { url: url + 'html' } }, { text: "JavaScript", web_app: { url: url + 'html' } }],
+                        [{ text: "React", web_app: { url: url + 'html' } }, { text: "Vue", web_app: { url: url + 'html' } }]
                     ]
                 }
             })
