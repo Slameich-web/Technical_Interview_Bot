3a80e869a52baae44b04ae6122176824f9d370f5
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-07 03:55:38 +0300

    fix 6

--- client/src/components/category/HtmlPage.jsx
+++ client/src/components/category/HtmlPage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "antd/dist/antd.css";
-import { html } from "../../data/html";
+import { Button } from "../button/Button";
 export const HtmlPage = () => {
   return (
     <div>
       <h2>Добро пожаловать на страницу с вопросами по HTML</h2>
       <h4>Тут ты можешь выбрать сложность вопросов и дополнительные темы</h4>
-      {html.map((item) => {
-        return <div key={item.id}>{item.title}</div>;
-      })}
+      <Button>Для стажеров</Button>
+      <Button>Для junior</Button>
     </div>
   );
 };
