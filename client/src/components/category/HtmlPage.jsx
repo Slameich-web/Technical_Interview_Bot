f14e7c51f9aa7af6192a32794ca1ab6023bd5f18
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-07 03:31:38 +0300

    fix

--- client/src/components/category/HtmlPage.jsx
+++ client/src/components/category/HtmlPage.jsx
@@ -7,12 +7,15 @@ export const HtmlPage = () => {
   Known for its loyalty and faithfulness,
   it can be found as a welcome guest in many households across the world.
 `;
+  const { Panel } = Collapse;
   return (
     <div>
       <h2>Добро пожаловать на страницу с вопросами по HTML</h2>
       <h4>Тут ты можешь выбрать сложность вопросов и дополнительные темы</h4>
-      <Collapse header="This is panel header 1" key="1">
-        <p>{text}</p>
+      <Collapse defaultActiveKey={["1"]}>
+        <Panel header="This is panel header 1" key="1">
+          <p>{text}</p>
+        </Panel>
       </Collapse>
     </div>
   );
