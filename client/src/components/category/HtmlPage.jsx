f89b1b5250e9cf4ff39334751aed3cdb1591f19c
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-13 23:45:49 +0300

    level

--- client/src/components/category/HtmlPage.jsx
+++ client/src/components/category/HtmlPage.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import "antd/dist/antd.css";
+import "Page.css";
 import { Button } from "../button/Button";
 export const HtmlPage = () => {
   return (
     <div>
       <h2>Добро пожаловать на страницу с вопросами по HTML</h2>
       <h4>Выбирай свой уровень и готовься к собеседованию</h4>
-      <Button>Cтажеров</Button>
-      <Button>Junior</Button>
+      <div className="choice_level_button">
+        <Button>Начальный уровень</Button>
+        <Button>Средний уровень</Button>
+        <Button>Высокий уровень</Button>
+      </div>
     </div>
   );
 };
