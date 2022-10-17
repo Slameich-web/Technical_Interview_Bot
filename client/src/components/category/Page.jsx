9278350e341f8129775aa41b942063a14312b2ef
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-18 00:34:17 +0300

    fix 3

--- client/src/components/category/Page.jsx
+++ client/src/components/category/Page.jsx
@@ -2,14 +2,28 @@ import React from "react";
 import "./Page.css";
 import { Button } from "../button/Button";
 import { react } from "../../data/react";
+import { useState } from "react";
+import { useTelegram } from "../../hooks/useTelegram";
 export const Page = ({ title }) => {
+  const { TG } = useTelegram();
+  const [category, setCategory] = useState(undefined);
+  if (category) {
+    return (
+      <div>
+        {category.map(({ title }) => {
+          return <div>{title}</div>;
+        })}
+        {TG.MainButton.show()}
+      </div>
+    );
+  }
   return (
     <div>
       <div>{title}</div>
-      {react.map(({ title, description }) => {
+      {react.map(({ title, description, questions }) => {
         return (
           <div className="choice_level_button">
-            <Button>{title}</Button>
+            <Button onClick={() => setCategory(questions)}>{title}</Button>
             <p>{description}</p>
           </div>
         );
