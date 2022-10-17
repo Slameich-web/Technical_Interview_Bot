ac36af681c456ec20cc38edcb7f168a1c876ae1e
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-18 00:19:38 +0300

    fix

--- client/src/components/category/Page.jsx
+++ client/src/components/category/Page.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./Page.css";
 import { Button } from "../button/Button";
-
+import { react } from "../../data/react";
 export const Page = ({ title }) => {
-  const data = React.lazy(() => import(`../../data/${title}`));
-  console.log(data);
   return (
     <div>
       <div>{title}</div>
-      {data.map(({ title, description }) => {
+      {react.map(({ title, description }) => {
         return (
           <div className="choice_level_button">
             <Button>{title}</Button>
