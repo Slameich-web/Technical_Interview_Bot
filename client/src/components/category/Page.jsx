a0a7b37910be1d0fb345af57a1e602817945ff3e
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-17 23:13:19 +0300

    fix

--- client/src/components/category/Page.jsx
+++ client/src/components/category/Page.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Page.css";
-import { Button } from "../button/Button";
 import { react } from "../../data/react";
 export const Page = ({ title }) => {
   return (
