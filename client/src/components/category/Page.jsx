f11c734de665bc88ee6957e68811c3b165b822e1
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-17 23:15:32 +0300

    div => button

--- client/src/components/category/Page.jsx
+++ client/src/components/category/Page.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Page.css";
+import { Button } from "../button/Button";
 import { react } from "../../data/react";
 export const Page = ({ title }) => {
   return (
     <div>
       {react.map(({ title }) => {
-        return <div>{title}</div>;
+        return <Button>{title}</Button>;
       })}
     </div>
   );
