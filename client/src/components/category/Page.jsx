9370c4847f7d1ae60725e560aa863dc689ffbb89
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-23 21:13:54 +0300

    MainButton

--- client/src/components/category/Page.jsx
+++ client/src/components/category/Page.jsx
@@ -12,7 +12,7 @@ export const Page = ({ title }) => {
   TG.onEvent("mainButtonClicked", () => setActiveQuestion(activeQuestion + 1));
   if (category) {
     TG.MainButton.show();
-    TG.MainButton.setText("text");
+    TG.MainButton.setText(`Перейти к вопросу № ${activeQuestion}`);
     return (
       <div>
         <div>
