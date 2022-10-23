c88a5bd7671303227d939dbb4bab0d9f05610be2
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-23 18:19:28 +0300

    fix

--- client/src/hooks/useTelegram.js
+++ client/src/hooks/useTelegram.js
@@ -4,15 +4,9 @@ export function useTelegram() {
     const onClose = () => {
         TG.close();
     };
-    const onChangeTextButton = (text) => {
-        if (text) {
-            TG.MainButton.setText(text)
-        }
-    };
     return {
         TG,
         onClose,
-        onChangeTextButton,
         user: TG.initDataUnsafe?.user
     }
 }
