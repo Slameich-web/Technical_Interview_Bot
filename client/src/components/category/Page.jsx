9f89dc1db8c8d777a16bc34af6c2bcfaf2124542
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-25 11:15:53 +0300

    refactoring 1

--- client/src/components/category/Page.jsx
+++ client/src/components/category/Page.jsx
@@ -5,18 +5,13 @@ import { Button } from "../button/Button";
 import { react } from "../../data/react";
 import { useState } from "react";
 import { useTelegram } from "../../hooks/useTelegram";
+import { Question } from "../question/Question";
 
 export const Page = ({ title }) => {
   const { TG } = useTelegram();
   const [category, setCategory] = useState(undefined);
   const [activeQuestion, setActiveQuestion] = useState(undefined);
   TG.onEvent("mainButtonClicked", () => setActiveQuestion(activeQuestion + 1));
-  const backButtonFunction = () => {
-    setActiveQuestion(undefined);
-    setCategory(undefined);
-    TG.MainButton.hide();
-    TG.BackButton.hide();
-  };
   if (category && activeQuestion === undefined) {
     TG.MainButton.hide();
     TG.BackButton.show();
@@ -33,28 +28,12 @@ export const Page = ({ title }) => {
     );
   }
   if (category) {
-    TG.MainButton.show();
-    TG.BackButton.show();
-    TG.MainButton.setText(`Перейти к вопросу № ${activeQuestion + 2}`);
-    TG.onEvent("backButtonClicked", () => backButtonFunction());
-
-    if (activeQuestion === category.length - 1) {
-      TG.onEvent("mainButtonClicked", () => setActiveQuestion(undefined));
-      TG.MainButton.show();
-      TG.MainButton.setText(`Перейти к списку вопросов`);
-    }
-
-    return (
-      <div>
-        <div>
-          <h2>
-            Вопрос № {activeQuestion + 1} <br />
-            {category[activeQuestion].title}
-          </h2>
-          <div>{category[activeQuestion].answer}</div>
-        </div>
-      </div>
-    );
+    <Question
+      setActiveQuestion={setActiveQuestion}
+      setCategory={setCategory}
+      activeQuestion={activeQuestion}
+      category={category}
+    />;
   }
 
   return (
--- client/src/components/question/Question.jsx
+++ client/src/components/question/Question.jsx
@@ -1,5 +1,44 @@
 import React from "react";
+import { useEffect } from "react";
+import { useTelegram } from "../../hooks/useTelegram";
 
-export const Question = () => {
-  return <div>Question</div>;
+const backButtonFunction = (TG, setActiveQuestion, setCategory) => {
+  setActiveQuestion(undefined);
+  setCategory(undefined);
+  TG.MainButton.hide();
+  TG.BackButton.hide();
+};
+
+export const Question = ({
+  setActiveQuestion,
+  setCategory,
+  activeQuestion,
+  category,
+}) => {
+  const { TG } = useTelegram();
+  TG.MainButton.show();
+  TG.BackButton.show();
+  TG.MainButton.setText(`Перейти к вопросу № ${activeQuestion + 2}`);
+  TG.onEvent("backButtonClicked", () =>
+    backButtonFunction(TG, setActiveQuestion, setCategory)
+  );
+  useEffect(() => {
+    if (activeQuestion === category.length - 1) {
+      TG.onEvent("mainButtonClicked", () => setActiveQuestion(undefined));
+      TG.MainButton.show();
+      TG.MainButton.setText(`Перейти к списку вопросов`);
+    }
+  }, [TG, activeQuestion, category.length, setActiveQuestion]);
+
+  return (
+    <div>
+      <div>
+        <h2>
+          Вопрос № {activeQuestion + 1} <br />
+          {category[activeQuestion].title}
+        </h2>
+        <div>{category[activeQuestion].answer}</div>
+      </div>
+    </div>
+  );
 };
